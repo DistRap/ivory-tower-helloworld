c0c54a61f685d286d80b94197019dbe4a04e1393
Richard Marko <srk@48.io>
2020-03-03 16:43:28 +0100

    bluepill: fix max7219 import

--- test/BluepillTest.hs
+++ test/BluepillTest.hs
@@ -4,9 +4,11 @@ import Hello.Tests.Platforms
 import Hello.Tests.Bluepill (app)
 
 import qualified Ivory.BSP.STM32F103 as F103
+import qualified Hello.Tests.Platforms.IOT01A as IOT
 
+bluepillWithDisplay :: Platform
 bluepillWithDisplay = bluepill {
-    platformSPIDevs  = [ max7219dev F103.spi1 F103.pinA4 ]
+    platformSPIDevs  = [ IOT.max7219dev F103.spi1 F103.pinA4 ]
   }
 
 main :: IO ()
