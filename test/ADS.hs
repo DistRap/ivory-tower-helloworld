c3813d391689dcc23d8efadafc7243584fd31e23
sorki <srk@48.io>
2024-02-17 09:03:45 +0100

    test/ADS: shave off type ann warning

--- test/ADS.hs
+++ test/ADS.hs
@@ -6,9 +6,11 @@ import Ivory.BSP.STM32L431
 import Hello.Tests.ADS (app)
 
 -- external I2C0
+p :: Platform
 p = monstick {
     platformI2C = i2c3
   , platformI2CPins = i2c3Pins
   }
+
 main :: IO ()
 main = buildHelloApp p app
