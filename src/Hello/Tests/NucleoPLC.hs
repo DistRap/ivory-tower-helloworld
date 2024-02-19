b93d39413c8f693b8fe1ad1fc0738df19e291cb7
sorki <srk@48.io>
2024-02-19 08:13:05 +0100

    finish NucleoPLC test

--- src/Hello/Tests/NucleoPLC.hs
+++ src/Hello/Tests/NucleoPLC.hs
@@ -6,13 +6,15 @@
 module Hello.Tests.NucleoPLC where
 
 import Ivory.Language
+import Ivory.Stdlib
 import Ivory.Tower
 
 import Hello.Tests.Platforms
 import Hello.Tests.Platforms.NucleoG474
 import Ivory.BSP.STM32.ClockConfig
 
-import Ivory.Tower.Base
+import Ivory.Tower.Base.UART
+import Ivory.Tower.Base.UART.Types
 
 import Ivory.BSP.STM32.Driver.SPI
 import Ivory.Tower.Drivers.IO.PLC01A1
@@ -22,17 +24,54 @@ app tocc toPlatform = do
   Platform{..} <- fmap toPlatform getEnv
 
   (sreq, sready) <- spiTower tocc platformSPIDevs platformSPIPins
-  (plcIn, plcOut) <- plcTower sreq sready vniOutEnablePin
+  (plcInputs, plcSetOutput, plcOutputStatus) <- plcTower sreq sready vniOutEnablePin
 
-  return ()
-  --p <- period (Seconds 1)
-  --monitor "plctest" $ do
-  --  cnt <- stateInit "counter" (ival (0 :: Uint8))
-  --  handler p "plcPeriod" $ do
-  --    plcOutE <- emitter plcOut 1
-  --    callback $ const $ do
-  --      emit plcOutE (constRef cnt)
+  uartTowerDeps
+  (ostream, istream) <- bufferedUartTower tocc platformUART platformUARTPins 115200 (Proxy :: Proxy UARTBuffer)
 
-  --ioIn <- io595 sreq (SPIDeviceHandle 0)
-  --cOut <- periodicCounter (Milliseconds 1000) (0 :: Uint8)
-  --fwd cOut ioIn
+  -- echo prompt doesn't control anything for now
+  dummyToggle <- channel
+  echoPrompt "hello plc world" ostream istream (fst dummyToggle)
+
+  p <- period (Milliseconds 50)
+  monitor "plctest" $ do
+    cnt <- stateInit "counter" (ival (0 :: Uint8))
+    outputs <- state "outputs"
+
+    handler p "plcSetOutputsPeriod" $ do
+      plcSetOutE <- emitter plcSetOutput 1
+      callback $ const $ do
+        pos <- deref cnt
+        -- flip a bit at current position
+        cur <- deref (outputs ! toIx pos)
+        store (outputs ! toIx pos) (iNot cur)
+
+        emit plcSetOutE (constRef outputs)
+        ifte_
+          (pos ==? 7)
+          (store cnt 0)
+          (cnt += 1)
+
+    outputsStatus <- state "outputsStatus"
+
+    handler plcOutputStatus "plcOutputStatus" $ do
+      o <- emitter ostream 64
+      callback $ \os -> do
+        vniFault <- vniHasFaults os
+        -- note that one fault is expected
+        -- on power on, until DC/DC converter
+        -- stabilizes
+        when vniFault $
+          puts o $ "VNI Fault\n"
+        refCopy outputsStatus os
+
+    inputs <- state "inputs"
+
+    handler plcInputs "plcInputs" $ do
+      o <- emitter ostream 64
+      callback $ \ins -> do
+        cltFault <- cltHasFaults ins
+        when cltFault $
+          puts o $ "CLT Fault\n"
+
+        refCopy inputs ins
