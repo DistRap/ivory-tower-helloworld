b511eb27ef80493fed1c0f272a2c5a34226371e8
sorki <srk@48.io>
2025-07-10 16:23:22 +0200

    ads-test: adapt to new driver

--- src/Hello/Tests/ADS.hs
+++ src/Hello/Tests/ADS.hs
@@ -32,7 +32,12 @@ app tocc toPlatform = do
   (i2cTransmit, ready) <- i2cTower tocc platformI2C platformI2CPins
   togIn <- ledToggle [platformRedLED]
 
-  (BackpressureTransmit adsRequest adsResult) <- adsTower i2cTransmit ready adsDefaultAddr
+  (BackpressureTransmit adsRequest adsResult) <-
+    adsTower
+      i2cTransmit
+      ready
+      adsDefaultAddr
+      configADS101xAllChannels
 
   per <- period (Milliseconds 1000)
   fwd per togIn
@@ -41,14 +46,21 @@ app tocc toPlatform = do
   fwd ms100 adsRequest
 
   uartTowerDeps
-  (ostream, _istream) <- bufferedUartTower tocc platformUART platformUARTPins 115200 (Proxy :: Proxy UARTBuffer)
+  (ostream, _istream) <-
+    bufferedUartTower
+      tocc
+      platformUART
+      platformUARTPins
+      115200
+      (Proxy :: Proxy UARTBuffer)
+
   monitor "dumpADS" $ do
     handler adsResult "adsResult" $ do
       o <- emitter ostream 64
       callback $ \adsSample -> do
         arrayMap $ \i -> do
           c <- deref (adsSample ! i)
-          (str :: Ref ('Stack s) UARTBuffer) <- uint32ToString $ safeCast c
+          (str :: Ref ('Stack s) UARTBuffer) <- floatingToString c 4
           putIvoryString o (constRef str)
           puts o " "
         puts o "\r"
